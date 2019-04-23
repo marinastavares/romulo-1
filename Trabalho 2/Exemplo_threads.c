181b9cc38443e5d24b93e7962ea9b2abab1694d6
marinastavares <marinastavares6@gmail.com>
2019-04-23 09:21:55 -0300

    adicionado logica para terminar threads

--- Trabalho 2/Exemplo_threads.c
+++ Trabalho 2/Exemplo_threads.c
@@ -5,27 +5,37 @@
 #include <pthread.h>
 
 pthread_t t1,t2,t3;
+int continua;
 
 void main_t1(void) {
-    while(1) {
+    while(continua) {
         printf("thread 1\n");
         sleep(1);
     }
 }
 
 void main_t2(void) {
-    while(1) {
+    while(continua) {
         printf("thread 22\n");
         sleep(2);
     }
 }
 
+void main_t3(void) {
+	 printf("Tecle algo para terminar\n");
+     getchar();   
+     continua = 0;
+}
+
 void main() {
     printf("Thread 1");
+    continua = 1;
 
     pthread_create(&t1, NULL, (void *) main_t1, NULL);
 
     pthread_create(&t2, NULL, (void *) main_t2, NULL);
+
+	pthread_create(&t3, NULL, (void *) main_t3, NULL);
 
     pthread_join(t1, NULL);
     pthread_join(t2, NULL);
