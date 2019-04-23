aac9da390b2560ab61c500625e04ec83889c7000
marinastavares <marinastavares6@gmail.com>
2019-04-23 09:15:11 -0300

    rodou com -lpthread

--- Trabalho 2/Exemplo_threads.c
+++ Trabalho 2/Exemplo_threads.c
@@ -13,8 +13,23 @@ void main_t1(void) {
     }
 }
 
+void main_t2(void) {
+    while(1) {
+        printf("thread 22\n");
+        sleep(2);
+    }
+}
+
 void main() {
     printf("Thread 1");
 
     pthread_create(&t1, NULL, (void *) main_t1, NULL);
+
+    pthread_create(&t2, NULL, (void *) main_t2, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    printf("Fim");
 }
+
